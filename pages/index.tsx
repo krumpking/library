30cb0640ac906924aa8db639627a7d9f950a817b
fmudondo <fmudondo@gmail.com>
2023-04-04 10:54:30 +0200

    Fixes
    Fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,10 @@
-import type { NextPage } from 'next'
-import React, { useState, useEffect } from 'react'
-<<<<<<< Updated upstream
-import Main from '../app/components/pages/main';
+import type { NextPage } from 'next';
+import React, { useState, useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../app/store/hooks';
 import { selectPosition } from '../app/store/reducer';
-=======
->>>>>>> Stashed changes
-
-
-
 
 const Home: NextPage = () => {
+  return <div>Hello welcome to VIP template</div>;
+};
 
-
-
-
-  return (
-    <div>
-      Hello welcome to VIP template
-    </div>
-  )
-}
-
-export default Home
+export default Home;
