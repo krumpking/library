65f41e61435ca40636f466298867a082a0cb3de0
Ishemunyoro <125468074+Ishemunyoro@users.noreply.github.com>
2023-06-15 17:01:24 +0300

    Home page update
    -- updated home page welcome message

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch, useAppSelector } from '../app/store/hooks';
 import { selectPosition } from '../app/store/reducer';
 
 const Home: NextPage = () => {
-  return <div>Hello welcome to VIP template</div>;
+  return <div>Hello welcome to VIP template  I changed this part</div>;
 };
 
 export default Home;
